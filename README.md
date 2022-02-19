b3966ef80d65e0597fc72cff198a06adb23e0be4
waleedmagdy <eng.waleedmagdy@gmail.com>
2022-02-19 15:49:10 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+# Temperature Converter CLI
+
+Converts temperature between Fahrenheit and Celsius.
+
+## Usage
+
+Compile with `go build -o temp`.
+
+Then, invoke the binary passing as argument the unit of temperature we want to convert **from**.
+For example:
+
+`./temp C` to convert from Celsius to Fahrenheit or `./temp F` to convert from Fahrenheit to Celsius.
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var originUnit string
+var originValue float64
+
+var shouldConvertAgain string
+
+var err error
+
+var errInvalidArguments = errors.New("Invalid arguments")
+var errReadingInput = errors.New("Error reading input")
+
+func main() {
+	if len(os.Args) != 2 {
+		printError(errInvalidArguments)
+	}
+
+	originUnit = strings.ToUpper(os.Args[1])
+
+	for {
+		fmt.Print("What is the current temperature in " + originUnit + " ? ")
+
+		_, err = fmt.Scanln(&originValue)
+
+		if err != nil {
+			printError(errReadingInput)
+		}
+
+		if originUnit == "C" {
+			convertToFahrenheit(originValue)
+		} else {
+			convertToCelsius(originValue)
+		}
+
+		fmt.Print("Would you like to convert another temperature ? (y/n) ")
+
+		_, err = fmt.Scanln(&shouldConvertAgain)
+
+		if err != nil {
+			printError(errReadingInput)
+		}
+
+		if strings.ToUpper(strings.TrimSpace(shouldConvertAgain)) != "Y" {
+			fmt.Println("Good bye!")
+			break
+		}
+	}
+}
+
+func printError(err error) {
+	fmt.Fprintf(os.Stderr, "error: %v\n", err)
+	os.Exit(1)
+}
+
+func convertToCelsius(value float64) {
+	convertedValue := (value - 32) * 5 / 9
+	fmt.Printf("%v F = %.0f C\n", value, convertedValue)
+}
+
+func convertToFahrenheit(value float64) {
+	convertedValue := (value * 9 / 5) + 32
+	fmt.Printf("%v C = %.0f F\n", value, convertedValue)
+}
